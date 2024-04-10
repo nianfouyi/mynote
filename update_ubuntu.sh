227196b9e425f8ff5637a64ec36d315022407245
nianfouyi <876217348@qq.com>
2024-04-11 07:02:15 +0800

    remove snapd and install firfox

--- update_ubuntu.sh
+++ update_ubuntu.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-# sudo apt remove -y deja-dup open-vm-tools remmina libreoffice-common transmission-common aisleriot gnome-mahjongg gnome-mines gnome-sudoku rhythmbox thunderbird && sudo apt autoremove gnome-todo -y
-# sudo apt-get autoremove
+sudo apt remove -y deja-dup open-vm-tools remmina libreoffice-common transmission-common aisleriot gnome-mahjongg gnome-mines gnome-sudoku rhythmbox thunderbird && sudo apt autoremove gnome-todo -y
+sudo apt-get autoremove
 
 
 #  列出并卸载所有通过 snap 安装的软件
@@ -29,10 +29,6 @@ sudo rm -rf ~/snap
 sudo find / -type d -name "*snap*" -exec rm -rf {} + 2>/dev/null
 
 
-rm -rf ~/snap
-sudo rm -rf /snap
-sudo rm -rf /var/snap
-sudo rm -rf /var/lib/snapd
 
 
 # 创建配置文件以阻止 apt 再次安装 snapd
@@ -44,3 +40,39 @@ Pin-Priority: -10
 EOF
 
 echo "操作完成。"
+
+# 安装firfox
+
+# 首先要判断哈是否有wget命令，没有的话需要先安装
+
+
+
+# 检查wget是否已安装
+if ! command -v wget &> /dev/null
+then
+    echo "wget could not be found, installing..."
+    sudo apt update
+    sudo apt install wget -y
+else
+    echo "wget is already installed."
+fi
+
+
+sudo install -d -m 0755 /etc/apt/keyrings
+
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+
+gpg -n -q --import --import-options import-show /etc/apt/keyrings/packages.mozilla.org.asc | awk '/pub/{getline; gsub(/^ +| +$/,""); if($0 == "35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3") print "\nThe key fingerprint matches ("$0").\n"; else print "\nVerification failed: the fingerprint ("$0") does not match the expected one.\n"}'
+
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+
+
+echo '
+Package: *
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+' | sudo tee /etc/apt/preferences.d/mozilla
+
+sudo apt-get update && sudo apt-get install firefox
+# 这里待确定
+sudo apt-get install firefox-l10n-zh-cn
